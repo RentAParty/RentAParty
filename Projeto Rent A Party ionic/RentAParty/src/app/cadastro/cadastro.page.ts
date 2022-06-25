52c6b53bab1e3e590deb4c66a3f6688a487fed0d
Matheus Garvão <matheusgarvao@edu.unifil.br>
2022-06-25 00:54:45 -0300

    Typescript login finalizado
    Typescript da página de login sem a implementação da database feito, além de algumas mudanças realizadas em algumas outras páginas.

--- Projeto Rent A Party ionic/RentAParty/src/app/cadastro/cadastro.page.ts
+++ Projeto Rent A Party ionic/RentAParty/src/app/cadastro/cadastro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro.page.scss'],
 })
 export class CadastroPage implements OnInit {
-
-  constructor() { }
+  regexemail = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  regexsenha = new RegExp(/^ (?=.* [A - Za - z])(?=.*\d)[A - Za - z\d]{ 8, }$/)
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
   }
+  
+ /* if (this.regexemail.test(this.email) == false) {
+    this.alertController.create({
+      header: 'ESPERA AI!!!',
+      subHeader: 'Email inválido',
+      message: 'Por favor, insira um endereço de email válido!',
+      buttons: ['OK']
+    }).then(resposta => {
+      resposta.present();
+    });
+  } else if (this.regexsenha.test(this.senha)==false) {
 
+    alert(this.senha)
+    this.alertController.create({
+      header: 'ESPERA AI!!!',
+      subHeader: 'Senha inválida',
+      message: 'A senha deve conter ao menos 8 caracteres e no máximo 20. Deve conter também uma letra e um número!',
+      buttons: ['OK']
+    }).then(resposta => {
+      resposta.present();
+    });
+  }*/
 }
--- Projeto Rent A Party ionic/RentAParty/src/app/login/login.page.ts
+++ Projeto Rent A Party ionic/RentAParty/src/app/login/login.page.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  constructor() { }
+  constructor(
+    private route: Router,
+    private alertController: AlertController) { }
 
-  ngOnInit() {
+  email: string;
+  senha: string;
+  cli: boolean;
+  fun: boolean;
+
+  validar(numero: number) {
+    switch (numero) {
+      case 1:
+        this.fun = false
+        break;
+      case 2:
+        this.cli = false
+        break;
+    }
   }
 
+  proximapag() {
+
+    if (this.email == "" || this.email==undefined) {
+      this.alertController.create({
+        header: 'ESPERA AI!!!',
+        subHeader: 'Endereço de email vazio!',
+        message: 'Por favor, preencha o seu endereço de email.',
+        buttons: ['OK']
+      }).then(res => {
+        res.present();
+      });
+
+    } else if (this.senha == "" || this.senha==undefined) {
+
+      this.alertController.create({
+        header: 'ESPERA AI!!!',
+        subHeader: 'Senha vazia!',
+        message: 'Por favor, insira a sua senha!',
+        buttons: ['OK']
+      }).then(res => {
+        res.present();
+      });
+
+    } else if (this.cli == true) {
+      this.route.navigate(['./cliente']);
+
+    } else if (this.fun == true) {
+      this.route.navigate(['./funcionario']);
+
+    } else {
+      this.alertController.create({
+        header: 'ESPERA AI!!!',
+        subHeader: 'O que você é?',
+        message: 'Selecione uma das opções entre cliente ou funcionário.',
+        buttons: ['OK']
+      }).then(res => {
+        res.present();
+      });
+    }
+  }
 }
+
+
+
